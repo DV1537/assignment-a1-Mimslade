c625c3a27b1de105f52be07f627b5c24bbfc6c94
Mimslade <Anton.maxen@gmail.com>
2018-11-17 18:30:46 +0100

    assigntment a1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,86 @@
 #include <iostream>
 #include <fstream>
-
-/**
- * The program reads numbers from a file and prints the sum of them
- * Example: main.exe ./tests/input.in
- * @param  argc [description]
- * @param  argv [description]
- * @return      [description]
- */
+#include <ctime>
+#include <cstdlib>
 int main(int argc, const char * argv[])
 {
-    
-    int a = 0;
-    int sum = 0;
-    
-    std::ifstream myReadFile;
-    
-    myReadFile.open(argv[1]);
-    
-    while (myReadFile >> a)
-    {
-        sum += a;
-    }
-    myReadFile.close();
-    
-    
-    
-    std::cout << sum << "\n";
-    
-    return 0;
-}
+  srand(time(NULL)); // random seed;
+  int number;
+  int counter;
+  int *iptr;
+  int accumulator;
+  int average;
+  std::string filename;
 
+  std::ifstream inputFile;
+  std::ofstream outputFile;
+  if(argc < 2) // if no argument other then filepath passed, creates own file with numbers for testing.
+  {
+    std::cout << "Since you did not specify what file you wanted to open, i created one for you that generates some random numbers.\n";
+    const int min = 1;
+    const int max = 10;
+    int namb;
+
+    filename = "noarg.txt";
+
+    namb = (rand() % (max - min + 1)) + min; // decides how many numbers in file.
+    outputFile.open(filename); // open inputFile.
+    if(outputFile.is_open())
+    {
+      for(int i = 0; i < namb; i++)
+      {
+        outputFile << (rand() % (max - min + 1)) + min << " "; // decides what numbers stored in file.
+      }
+      outputFile.close(); //close outputFile.
+    }
+    else
+      return EXIT_FAILURE;
+  }
+  else
+    filename = argv[1];
+
+  inputFile.open(filename);
+
+  if(inputFile.is_open())
+  {
+    counter = 0;
+    while(inputFile >> number) //check how many numbers in file.
+      counter++;
+
+    if(counter != 0)
+    {
+      iptr = new int[counter]; //create dynamic array with size of numbers in file.
+
+      //reset read position
+      inputFile.clear();
+      inputFile.seekg(0, std::ios::beg);
+
+      accumulator = 0;
+      std::cout << "Input: ";
+      for(int i = 0; i < counter; i++)
+      {
+        inputFile >> iptr[i];
+        std::cout << iptr[i] << " ";
+        accumulator += iptr[i];
+      }
+      inputFile.close(); //close inputfile.
+    }
+    else
+      return EXIT_FAILURE;
+  }
+  else
+    return EXIT_FAILURE;
+
+  average = accumulator / counter;
+  std::cout << "\tOutput: ";
+
+  for(int i = 0; i < counter; i++)
+  {
+    if(iptr[i] > average)
+    {
+      std::cout << iptr[i] << " ";
+    }
+  }
+
+  return 0;
+}
